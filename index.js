f3c756bfbee545226dc39def6aae99be38948e7a
mdhainf20 <mdhanif115825@gmail.com>
2022-06-28 08:28:04 +0600

    updated

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qytn8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const serviceAccount = require('./apollo-hospital-jwt-token.json') 
-// const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); 
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); 
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
